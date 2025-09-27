61c4b69a9fe794ac6950ffe5f7297101772621e2
mhdSyam <anislestari2006@gmail.com>
2025-09-27 14:00:21 +0700

    wallet summary fix2

--- admin-be/migrations/20250701065556-create-post-categories.js
+++ admin-be/migrations/20250701065556-create-post-categories.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('post_categories', {
@@ -7,7 +8,7 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER.UNSIGNED 
+        type: Sequelize.INTEGER.UNSIGNED,
       },
       post_id: {
         type: Sequelize.INTEGER.UNSIGNED,
@@ -29,10 +30,20 @@ module.exports = {
         onUpdate: 'NO ACTION',
         onDelete: 'NO ACTION',
       },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+      },
     });
   },
 
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('post_categories');
-  }
+  },
 };
