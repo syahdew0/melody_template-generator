f2b8deff5ddd49b313a6b1ecabf2f06610070027
mhdSyam <anislestari2006@gmail.com>
2025-09-30 13:56:11 +0700

    update mlm

--- admin-be/migrations/20250611032231-create-menu.js
+++ admin-be/migrations/20250611032231-create-menu.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Menus', {
+    await queryInterface.createTable('menus', {
       id: {
         type: Sequelize.INTEGER,
         autoIncrement: true,
@@ -49,6 +49,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Menus');
+    await queryInterface.dropTable('menus');
   },
 };
--- admin-be/migrations/20250611032300-create-sub-menu.js
+++ admin-be/migrations/20250611032300-create-sub-menu.js
@@ -13,7 +13,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: true,
         references: {
-          model: 'Menus',
+          model: 'menus',
           key: 'id',
         },
         onUpdate: 'CASCADE',
