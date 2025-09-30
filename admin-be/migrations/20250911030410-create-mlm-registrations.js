9ed86267717e205627f031626294b67acc32049b
mhdSyam <anislestari2006@gmail.com>
2025-09-30 13:46:54 +0700

    update mlm

--- admin-be/migrations/20250911030410-create-mlm-registrations.js
+++ admin-be/migrations/20250911030410-create-mlm-registrations.js
@@ -13,7 +13,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'customers',
+          model: 'Customers',
           key: 'id',
         },
         onUpdate: 'NO ACTION',
@@ -33,7 +33,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: true,
         references: {
-          model: 'customers',
+          model: 'Customers',
           key: 'id',
         },
         onUpdate: 'NO ACTION',
--- admin-be/migrations/20250912082549-add-mlm-registrations.js
+++ admin-be/migrations/20250912082549-add-mlm-registrations.js
@@ -15,7 +15,7 @@ module.exports = {
       type: Sequelize.INTEGER,
       allowNull: true,
       references: {
-        model: 'customers',
+        model: 'Customers',
         key: 'id',
       },
       onUpdate: 'NO ACTION',
