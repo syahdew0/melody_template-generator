3f07956bdee8288a0d9c088cd1aa440e7d90be91
mhdSyam <anislestari2006@gmail.com>
2025-09-30 13:59:33 +0700

    update mlm

--- admin-be/migrations/20250611032231-create-menu.js
+++ admin-be/migrations/20250611032231-create-menu.js
@@ -21,7 +21,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: true,
         references: {
-          model: '',
+          model: 'menus',
           key: 'id',
         },
         onUpdate: 'NO ACTION',
