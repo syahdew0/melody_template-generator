d70da74c3bd069a9433773367682277c5534fd1d
mhdSyam <anislestari2006@gmail.com>
2025-09-30 09:11:13 +0700

    update

--- admin-be/routes/public.js
+++ admin-be/routes/public.js
@@ -1,6 +1,6 @@
-// routes/public.js
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 const { Theme, Website } = require('../models');
 
 router.get('/site-info', async (req, res) => {
@@ -22,10 +22,26 @@ router.get('/site-info', async (req, res) => {
     const apiUrl = process.env.API_URL || `${req.protocol}://${req.get('host')}`;
 
     res.json({ 
-      id: theme.id,name: theme.name || 'Default', slug: theme.slug || null, title, icon, apiUrl });
+      id: theme.id,
+      name: theme.name || 'Default',
+      slug: theme.slug || null,
+      title,
+      icon,
+      apiUrl 
+    });
   } catch (err) {
     console.error("Error site-info:", err);
     res.status(500).json({ success: false, message: 'Server error' });
+  }
+});
+
+router.get('/themes/manifest', async (req, res) => {
+  try {
+    const response = await axios.get('https://themes.phisoft.co.id/manifest.json');
+    res.json(response.data); // langsung dikirim ke frontend
+  } catch (err) {
+    console.error('Gagal ambil manifest:', err.message);
+    res.status(500).json({ success: false, message: 'Gagal ambil manifest' });
   }
 });
 
